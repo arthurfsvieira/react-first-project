95bc008a15645700a3948a0140be1e031d998990
Arthur Vieira <arthur.devbr@gmail.com>
2024-10-18 15:27:07 +0100

    logic operators

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ function Page (){
       <h1 className="text-bold text-3xl">Hello, World!</h1>
       <h1 className="italic">This is a React project with Next.</h1>
 
-      <Card phrase = "Essa fera aí, bicho!" />
+      <Card phrase = "Essa fera aí, bicho!" author = "Faustop" />
 
       <img className="border-4 border-dotted border-yellow-200" src="https://upload.wikimedia.org/wikipedia/en/9/91/The_Strokes_-_Comedown_Machine.jpg" alt="" />
 
