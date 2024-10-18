a1edb504ade8e78144760d35a79c3ae1f89b6a17
Arthur Vieira <arthur.devbr@gmail.com>
2024-10-18 14:58:44 +0100

    ternary condition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,7 @@ function Page (){
       <h1 className="text-bold text-3xl">Hello, World!</h1>
       <h1 className="italic">This is a React project with Next.</h1>
 
-      <Card>
-        <>
-          <h3 className="text-3xl font-bold text-center italic">"Essa fera aí, bicho!"</h3>
-          <p className="text-right text-sm">-Fausto Silva</p>
-        </>
-      </Card>
+      <Card phrase = "Essa fera aí, bicho!" />
 
       <img className="border-4 border-dotted border-yellow-200" src="https://upload.wikimedia.org/wikipedia/en/9/91/The_Strokes_-_Comedown_Machine.jpg" alt="" />
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,13 @@
-import { ReactNode } from "react";
-
 type Props = {
     phrase: string;
-    author: string;
+    author?: string;
 }
 
 export const Card = ({phrase, author}: Props) => {
     return (
         <div className="w-96 border-2 border-blue-300 p-3">
             <h3 className="text-3xl font-bold italic">"{phrase}"</h3>
-            <p className="text-right text=sm">- {author}</p>
+            <p className="text-right text=sm">- {author ? author : "Autor Desconhecido"}</p>
         </div>
     );
 }
